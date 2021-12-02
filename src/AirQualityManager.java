590d4cb63e6ff561aaedd88f31946a17c2050433
Lucy <lyonlg@hendrix.edu>
2021-12-02 09:11:28 +0100

    testing commit

--- src/AirQualityManager.java
+++ src/AirQualityManager.java
@@ -1,6 +1,6 @@
 public class AirQualityManager {
     // Main method
-    public static void main(String args[]){
-
+    public static void main(String[] args){
+        System.out.println("beep boop please run");
     }
 }
