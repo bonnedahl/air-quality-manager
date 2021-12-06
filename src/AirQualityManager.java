89f6c9180bda237a7958d594e929ce9f704184c0
Lucy <lyonlg@hendrix.edu>
2021-12-06 14:42:07 +0100

    deleted sensorpointdata - added Compound Enum

--- src/AirQualityManager.java
+++ src/AirQualityManager.java
@@ -1,4 +1,7 @@
 public class AirQualityManager {
     // Main method
-    public static void main(String[] args){ }
+    public static void main(String[] args){
+       //SensorDataPoint measurement = new SensorDataPoint(GasLabel.NO2, ) ;
+       String test = Compound.O3.getUnit();
+    }
 }
--- src/Sensor.java
+++
@@ -1,5 +0,0 @@
-class Sensor {
-    GasLabel test = GasLabel.NO2;
-    GasLabel test1 = GasLabel.NO2;
-
-}
