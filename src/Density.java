f5a3bdf2c33390f7495697502bbce73d67ef3527
Ahmad <ahmadjamlo97@gmail.com>
2021-12-06 14:45:33 +0100

    asd

---
+++ src/Density.java
@@ -0,0 +1,18 @@
+public class Density extends Attribute {
+    
+    private Compound compound;
+    private float value;
+
+    public Density(String compound, float value) {
+        this.compound = compound;
+        this.value = value;
+    }
+
+    public String getCompound() {
+        return compound;
+    }
+
+    public float getValue() {
+        return value;
+    }
+}
--- src/Measurement.java
+++ src/Measurement.java
@@ -1,3 +1,28 @@
+import java.util.*;
+
 public class Measurement {
 
+    private Date timestamp;
+    private Sensor sensor;
+    private Density[] values;
+
+    // We assume that a sensor always measures the 4 different gases at the same time
+
+    public Measurement(Date timestamp, Sensor sensor, Density[] values) {
+        this.timestamp = timestamp;
+        this.sensor = sensor;
+        this.values = values;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public Density[] getValues() {
+        return values;
+    }
 }
